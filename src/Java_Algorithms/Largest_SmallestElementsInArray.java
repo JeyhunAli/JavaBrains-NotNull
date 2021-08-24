a1ff24e11b3f9b219b1d63259f0070367a227335
Jey <jey@Jeys-MacBook-Pro.local>
2021-08-24 16:30:37 -0400

    new classes

--- src/Java_Algorithms/Largest_SmallestElementsInArray.java
+++ src/Java_Algorithms/Largest_SmallestElementsInArray.java
@@ -3,8 +3,35 @@ package Java_Algorithms;
 public class Largest_SmallestElementsInArray {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	
+		//finding smallest and largest number in array 
+		
+		
+		
+		int num[] = {1,3,4,55,44,65,76,87,7,99,};
+		int largest = num[0];
+		int smallest = num[0];
+		for(int i=0; i<num.length; i++) {
+			if(num[i]> largest) {
+				largest = num[i];
+			}
+			else if(num[i]<smallest) {
+				smallest=num[i];
+			}
+		}
+		
+		
+		
+		System.out.println("largest " + largest);
+		System.out.println("smallest" + smallest);
+		
+		
+		
+		
+		
+		
+		
 	}
+	
 
 }
---
+++ src/Java_Algorithms/testing.java
@@ -0,0 +1,20 @@
+package Java_Algorithms;
+
+public class testing {
+
+	public static void main(String[] args) {
+		
+	//revrese String
+		
+		String s = "java";
+		String rev = "";
+		for(int i=s.length()-1; i>=0; i--) {
+			rev = rev+s.charAt(i);
+		}
+		System.out.println(rev);
+	}
+
+}
+
+
+
